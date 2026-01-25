c40cd17d1b7c6327eb7cb3267aee1dc1bbb3d2d2
MrSilvestr <silvemtrmilov@gmail.com>
2026-01-25 17:29:47 +0300

    translated into English

--- packit/src/cfg_comps/other.py
+++ packit/src/cfg_comps/other.py
@@ -11,24 +11,24 @@ class OtherSettings:
             Header(text=strings.other_settings),
             Switch(
                 key="show_chat_menu",
-                text="Кнопка в меню чата",
-                subtext="Добавляет кнопку настроек шаблонов в обычное меню чата.",
+                text="Button in chat menu",
+                subtext="Adds a template settings button to the regular chat menu.",
                 default=False,
                 icon="msg_settings",
                 on_change=self.chat_button.on_chat_switch if self.chat_button else None
             ),
             Switch(
                 key="show_drawer_menu",
-                text="Кнопка в боковом меню",
-                subtext="Добавляет кнопку настроек шаблонов в боковое меню.",
+                text="Button in side menu",
+                subtext="Adds a template settings button to the side menu.",
                 default=False,
                 icon="msg_info",
                 on_change=self.chat_button.on_drawer_switch if self.chat_button else None
             ),
             Switch(
                 key="show_chat_plugins_menu",
-                text="Кнопка в плагинах в чате",
-                subtext="Добавляет кнопку настроек шаблонов в меню плагинов в чате.",
+                text="Button in chat plugins",
+                subtext="Adds a template settings button to the chat plugins menu.",
                 default=False,
                 icon="msg_plugins",
                 on_change=self.chat_button.on_chat_plugins_switch if self.chat_button else None
