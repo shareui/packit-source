d4900676b3acd75679277c4709be73eeb1f7f01b
Vestr <silvemtrmilov@gmail.com>
2026-01-25 10:40:26 +0300

    added adding buttons to the chat menu, chat plugins menu and side menu

--- packit/src/cfg_comps/other.py
+++ packit/src/cfg_comps/other.py
@@ -1,19 +1,43 @@
-from ui.settings import Header, Selector
+from ui.settings import Header, Switch
 from elyx import strings, settings
 
 
 class OtherSettings:
-    def __init__(self):
-        pass
+    def __init__(self, chat_button=None):
+        self.chat_button = chat_button
     
     def build(self):
         return [
             Header(text=strings.other_settings),
-            Selector(
-                key="auto_update_interval",
+            Switch(
+                key="show_chat_menu",
+                text="Кнопка в меню чата",
+                subtext="Добавляет кнопку настроек шаблонов в обычное меню чата.",
+                default=False,
+                icon="msg_settings",
+                on_change=self.chat_button.on_chat_switch if self.chat_button else None
+            ),
+            Switch(
+                key="show_drawer_menu",
+                text="Кнопка в боковом меню",
+                subtext="Добавляет кнопку настроек шаблонов в боковое меню.",
+                default=False,
+                icon="msg_info",
+                on_change=self.chat_button.on_drawer_switch if self.chat_button else None
+            ),
+            Switch(
+                key="show_chat_plugins_menu",
+                text="Кнопка в плагинах в чате",
+                subtext="Добавляет кнопку настроек шаблонов в меню плагинов в чате.",
+                default=False,
+                icon="msg_plugins",
+                on_change=self.chat_button.on_chat_plugins_switch if self.chat_button else None
+            ),
+            Switch(
+                key="auto_update_on_start",
                 text="Auto-update",
-                default=0,
-                items=["Never", "30m", "1h", "4h", "8h", "12h", "24h"],
+                subtext="Update repos when starting app",
+                default=False,
                 icon="msg_retry"
             )
         ]
