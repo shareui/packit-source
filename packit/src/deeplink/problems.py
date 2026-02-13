411ad001fbf23b3f755a72b1fd4b09f0b9358400
shareui <shareui@proton.me>
2026-02-13 14:55:18 +0200

    impl tg://packit?forum and tg://packit?problems

--- packit/src/deeplink/problems.py
+++ packit/src/deeplink/problems.py
@@ -1,12 +1,15 @@
-from ui.bulletin import BulletinHelper
 from client_utils import get_last_fragment
-from android_utils import log
+from android.net import Uri
+from org.telegram.messenger.browser import Browser
 
-# должно открывать ссылку https://t.me/c/3663388991/13/350
+
 def handle(url):
     if url == "tg://packit?problems":
         try:
-            currentFragment = get_last_fragment()
-            BulletinHelper.show_success("Problems link triggered", currentFragment)
-        except Exception as e:
-            log(f"[PackIt] Error: {e}")
+            frag = get_last_fragment()
+            act = frag.getParentActivity() if frag else None
+            if act:
+                uri = Uri.parse("https://t.me/packitGround/13/350")
+                Browser.openUrl(act, uri, True, True, True, None, None, False, False, False)
+        except Exception:
+            pass
